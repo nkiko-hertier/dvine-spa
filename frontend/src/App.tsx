dbef4d71c51f02659541f969f95b54ffd7c0ee4d
Nkiko Hertier <afrigames123@gmail.com>
2026-08-21 11:46:45 +0200

    Make the app dashbaord based

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Services from "./pages/Serices";
-import Contact from "./pages/Contact";
-import Booking from "./pages/Booking";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Services from "./pages/Serices";
+// import Contact from "./pages/Contact";
+// import Booking from "./pages/Booking";
 import Login from "./pages/admin/Login";
 import Dashboard from "./pages/admin/Dashboard";
 import NotFound from "./pages/NotFound";
@@ -23,11 +23,11 @@ export default function App() {
         <BrowserRouter>
             
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
+                <Route path="/" element={<Login />} />
+                {/*<Route path="/about" element={<About />} />
                 <Route path="/services" element={<Services />} />
                 <Route path="/contact" element={<Contact />} />
-                <Route path="/booking" element={<Booking />} />
+                <Route path="/booking" element={<Booking />} />*/}
                 <Route path="/login" element={<Login />} />
                 <Route
                     path="/dashboard"
